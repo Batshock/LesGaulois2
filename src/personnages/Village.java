1611eefddb0c2a8f3d0fa6916192be8da7cce509
Dimitri Kouzmitch <kzd4327a@ens.fsi>
2023-03-14 09:03:47 +0100

    Save as 9h03

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -4,13 +4,14 @@ public class Village {
 	private String nom;
 	private Chef chef;
 	private int nbVillageoisMaximum;
-	private Gaulois villageois[] = new Gaulois[nbVillageoisMaximum];
+	private Gaulois villageois[];
 	private int nbVillageois = 0;
 	
 	
 	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
 		this.nbVillageoisMaximum = nbVillageoisMaximum;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
 	public void setChef(Chef chef) {
 		this.chef = chef;
@@ -28,15 +29,15 @@ public class Village {
 	
 	public static void main(String[] args) {
 		Village village = new Village("Village des irréductibles",30);
-//		Gaulois gaulois = village.trouverHabitant(1);
-//		On obtient cette erreur car on cherche un habitant dans un emplace où il n'y a pas de gaulois
+//		Gaulois gaulois = village.trouverHabitant(30);
+//		On obtient cette erreur car on dépasse la taille du village/tableau
 		Chef abraracourcix = new Chef("Abraracourcix",6,village);
 		village.setChef(abraracourcix);
 		Gaulois asterix = new Gaulois("Astérix",8);
 		village.ajouterHabitant(asterix);
-//		Gaulois gaulois = village.trouverHabitant(0);
-//		System.out.println(gaulois);
-//		On ne peut pas afficher un gaulois sans une fonction, sinon ça renvoie une adresse mémoire
+		Gaulois gaulois = village.trouverHabitant(1);
+		System.out.println(gaulois);
+//		Cela affiche "null" car on cherche dans un emplacement où il n'y a pas de gaulois, donc la variable gaulois ne vaut rien donc "null"
 		
 	}
 
